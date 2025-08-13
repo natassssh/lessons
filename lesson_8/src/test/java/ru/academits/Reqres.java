2b02ec6b271ac302a0750716e998800a570b3a9b
Gerasimova Natalia <atal.gerasimova@gmail.com>
2025-08-13 14:20:32 +0800

    Add lesson_8

---
+++ lesson_8/src/test/java/ru/academits/Reqres.java
@@ -0,0 +1,130 @@
+package ru.academits;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Reqres {
+
+    @Test
+    public void createUserWithParamInMap() {
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("name", "ivan");
+        requestBody.put("job", "lawyer");
+
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("https://reqres.in/api/users")
+                .andReturn();
+        response.prettyPrint();
+
+        Assertions.assertEquals(201, response.getStatusCode());
+        Assertions.assertEquals(requestBody.get("name"), response.jsonPath().getString("name"));
+    }
+
+
+    @Test
+    public void createUserWithParam() {
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body("{\n" +
+                            "\t\"name\": \"ivan\",\n" +
+                            "\t\"job\": \"lawyer\"\n" +
+                            "}")
+                .when()
+                .post("https://reqres.in/api/users")
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        System.out.println("Status code: " + statusCode);
+
+        assertEquals(201, statusCode, "Unexpected status code: expected 201, but was + statusCode");
+
+        System.out.println("Content-Type: " + response.header("Content-Type"));
+        System.out.println("Response: \n" + response.asPrettyString());
+
+        String[] expectedKeys = {"name", "job", "id", "createdAt"};
+
+        for (String expectedKey: expectedKeys) {
+            response.then().assertThat().body("$", hasKey(expectedKey));
+        }
+    }
+
+
+    @Test
+    public void testParseJson() {
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("name", "ivan");
+        requestBody.put("job", "lawyer");
+
+        JsonPath response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("https://reqres.in/api/users")
+                .jsonPath();
+
+        Assertions.assertEquals(requestBody.get("name"), response.get("name").toString());
+        Assertions.assertEquals(requestBody.get("job"), response.get("job").toString());
+    }
+
+
+    @Test // негативный, но отрабатывает
+    public void createUserWithoutBody() {
+        Map<String, String> requestBody = new HashMap<>();
+
+        ValidatableResponse response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(404);
+    }
+
+
+    @Test
+    public void testDate() {
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body("{\n" +
+                        "\t\"name\": \"ivan\",\n" +
+                        "\t\"job\": \"lawyer\"\n" +
+                        "}")
+                .when()
+                .post("https://reqres.in/api/users")
+                .andReturn();
+        response.prettyPrint();
+
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = now.format(formatter);
+
+        assertEquals(201, response.getStatusCode());
+        Assertions.assertTrue(response.jsonPath().getString("createdAt").contains(formattedDate));
+    }
+}
--- practical_tasks/src/ru/ais/java/SSS.java
+++ practical_tasks/src/ru/ais/java/SSS.java
@@ -2,7 +2,7 @@ package ru.ais.java;
 
 import java.util.Scanner;
 
-public class Main {
+public class SSS {
 
     public static void main(String[] args){
         System.out.println("Введите ваше имя");
